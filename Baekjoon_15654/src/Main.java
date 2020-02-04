2d1e0398134be03b3cc0523f95ab4e1117f856b7
YUP <mduddns@naver.com>
2020-02-04 13:39:07 +0900

    백준 15654 n과 m 5번 수열 사전 순 증가문제 백트래킹

---
+++ Baekjoon_15654/src/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	static int n;
+	static int m;
+	static int[] arr;
+	static int[] output;
+	static boolean[] visited;
+	static BufferedWriter bw;
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] tmp = br.readLine().split(" ");
+		n = Integer.parseInt(tmp[0]);
+		m = Integer.parseInt(tmp[1]);
+		tmp = br.readLine().split(" ");
+		arr = new int[n];
+		output = new int[m];
+		visited = new boolean[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(tmp[i]);
+		}
+		Arrays.sort(arr);
+		per(0,0);
+		bw.flush();
+	}
+	public static void per(int d, int c) throws IOException {
+		if(c==m) {
+			for(int k : output) {
+				bw.append(k+" ");
+			}
+			bw.append("\n");
+		}
+		else {
+			for(int i=0; i<n; i++) {
+				if(!visited[i]) {
+					visited[i] = true;
+					output[c] = arr[i];
+					per(d+1,c+1);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+
+}
