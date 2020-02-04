b0d355f524aaad074fd350824d24918fbacadad4
YUP <mduddns@naver.com>
2020-02-04 13:29:18 +0900

    백준 15652 n과  m 4 비내림차순 중복조합

---
+++ Baekjoon_15652/src/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+	static int n;
+	static int m;
+	static int[] arr;
+	static boolean[] visited;
+	static BufferedWriter bw;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n];
+		visited = new boolean[n];
+		int[] output = new int[m];
+		for(int i=0; i<n; i++) {
+			arr[i] = i+1;
+		}
+		comb(0,0,output);
+		bw.flush();
+	}
+	
+	public static void comb(int d, int c, int[] output) throws IOException {
+		if(c==m) {
+			for(int k : output) {
+				bw.append(k+" ");
+			}
+			bw.append("\n");
+		}
+		else {
+			for(int i=0; i<n; i++) {
+				if(d!=0) {
+					if(output[d-1]<=arr[i]) {
+						output[d] = arr[i];
+						comb(d+1,c+1,output);
+					}
+				}
+				else {
+					output[d] = arr[i];
+					comb(d+1,c+1,output);
+				}
+			}
+		}
+	}
+	
+
+}
